99ef3f972dad145666872208bb5686e9d22bef01
Ignacio Casal Quinteiro <qignacio@amazon.com>
2026-01-07 11:49:43 +0100

    Move resources to resources directory

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ use glib_build_tools::compile_resources;
 
 fn main() {
     compile_resources(
-        &["data"],
-        "data/resources.gresource.xml",
+        &["resources"],
+        "resources/resources.gresource.xml",
         "resources.gresource",
     );
 }
