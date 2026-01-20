c07ccacfe781811c1e386344fda5e3f930a24203
Ignacio Casal Quinteiro <qignacio@amazon.com>
2026-01-20 10:25:41 +0100

    Fix test race condition with mutex for environment variables

--- src/utils.rs
+++ src/utils.rs
@@ -14,39 +14,48 @@ pub fn language_from_locale() -> &'static str {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::sync::Mutex;
+
+    static TEST_MUTEX: Mutex<()> = Mutex::new(());
 
     #[test]
     fn test_language_from_locale_spanish() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "es_ES.UTF-8");
         assert_eq!(language_from_locale(), "es");
     }
 
     #[test]
     fn test_language_from_locale_italian() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "it_IT.UTF-8");
         assert_eq!(language_from_locale(), "it");
     }
 
     #[test]
     fn test_language_from_locale_english() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "en_US.UTF-8");
         assert_eq!(language_from_locale(), "us");
     }
 
     #[test]
     fn test_language_from_locale_default() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "xx_YY.UTF-8");
         assert_eq!(language_from_locale(), "us");
     }
 
     #[test]
     fn test_language_from_locale_polish() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "pl_PL.UTF-8");
         assert_eq!(language_from_locale(), "pl");
     }
 
     #[test]
     fn test_language_from_locale_partial_match() {
+        let _lock = TEST_MUTEX.lock().unwrap();
         std::env::set_var("LANG", "es");
         assert_eq!(language_from_locale(), "es");
     }
